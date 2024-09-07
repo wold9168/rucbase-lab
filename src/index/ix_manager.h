d57c6e5c679aaff790ab3397681915f76076eec0
wold9168 <moewold@outlook.com>
2024-09-08 05:18:56 +0800

    fix: Fix a dynamic memory error in Index Manager(ix_manager)

--- src/index/ix_manager.h
+++ src/index/ix_manager.h
@@ -163,5 +163,6 @@ class IxManager {
         // 缓冲区的所有页刷到磁盘，注意这句话必须写在close_file前面
         buffer_pool_manager_->flush_all_pages(ih->fd_);
         disk_manager_->close_file(ih->fd_);
+        delete[] data;
     }
 };
